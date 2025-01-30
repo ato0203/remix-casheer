8b649d48566dab7d3fb3f7213667adec85fbd61f
Ato Lie <atollie234@gmail.com>
2025-01-30 15:54:32 +0700

    Customize default layout

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import SiteNavbar from "./components/site-navbar";
 
 import "./tailwind.css";
 
@@ -24,14 +25,14 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -41,5 +42,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div className="min-h-full">
+      <SiteNavbar />
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 sm:py-8">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
