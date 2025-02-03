d4c2ca24fae7c8b855c404f1cbcc1922bc2a67fd
Ato Lie <atollie234@gmail.com>
2025-02-03 16:48:07 +0700

    Add initial UI for showing product detail

--- app/routes/products.$productId.tsx
+++ app/routes/products.$productId.tsx
@@ -1,7 +1,30 @@
+import { LoaderFunctionArgs } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import invariant from "tiny-invariant"
+import { getProduct } from "~/lib/db/fake-product"
+
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  invariant(params.productId, 'Missing productId params')
+  const product = await getProduct(params.productId)
+
+  if (!product) {
+    throw new Response('product not found', { status: 404 })
+  }
+
+  return product
+}
+
 export default function ProductDetail() {
+  const product = useLoaderData<typeof loader>()
+
   return (
     <div>
-      <h3>Product Detail</h3>
+      <h3>{product?.name}</h3>
+      <p>{product?.price}</p>
+      <p>{product?.stock}</p>
+      <h3>{product?.barcode}</h3>
     </div>
   )
 }
